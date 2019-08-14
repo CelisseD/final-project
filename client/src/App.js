ff627565d9eba4547c86bba9418df7ebea6a7285
CelisseD <43502934+CelisseD@users.noreply.github.com>
2019-08-13 21:23:58 -0400

    edits to components

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import LandingPage from "./components/Landing/landing";
@@ -9,55 +10,18 @@ import SearchPage from "./components/Search/search";
 import ResultsPage from "./components/Results/results";
 import DashboardPage from "./components/Dashboard/dashboard";
 import FavoritesPage from "./components/Favorites/favorites";
-
-import logo from "./logo.svg";
+import Navbar from "./components/Navbar/navbar";
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <div className="container">
-          <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand" href="http://google.com" target="_blank" rel="noopener noreferrer">
-              <img src={logo} width="30" height="30" alt="Home" />
-            </a>
-            <Link to="/" className="navbar-brand">TBH</Link>
-            <div className="collapse navbar-collapse">
-              <ul className="navbar-nav mr-auto">
-                <li className="navbar-item">
-                  <Link to="/" className="nav-link">Home</Link>
-                </li>
-                <li className="navbar-item">
-                  <Link to="/login" className="nav-link">Login</Link>
-                </li>
-                <li className="navbar-item">
-                  <Link to="/profile" className="nav-link">Profile</Link>
-                </li>
-                <li className="navbar-item">
-                  <Link to="/search" className="nav-link">Search</Link>
-                </li>
-                <li className="navbar-item">
-                  <Link to="/results" className="nav-link">Results</Link>
-                </li>
-                <li className="navbar-item">
-                  <Link to="/dashboard" className="nav-link">Dashboard</Link>
-                </li>
-                <li className="navbar-item">
-                  <Link to="/favorites" className="nav-link">Favorites</Link>
-                </li>
-              </ul>
-            </div>
-          </nav>
-
-          <h2>TBH App</h2>
-          <Route path="/" exact component={LandingPage} />
-          <Route path="/login" component={LoginPage} />
-          <Route path="/profile" component={ProfilePage} />
-          <Route path="/search" component={SearchPage} />
-          <Route path="/results" component={ResultsPage} />
-          <Route path="/dashboard" component={DashboardPage} />
-          <Route path="/favorites" component={FavoritesPage} />
-        </div>
+        <Navbar />
+        <Switch>
+          {/* <Route exact path="/" component={LandingPage} /> */}
+          {/* <Route exact path="/search" component={SearchPage} /> */}
+          {/* <Route exact path="/results" component={ResultsPage} /> */}
+        </Switch>
       </Router>
     )
   }
---
+++ client/src/components/Navbar/navbar.js
@@ -0,0 +1,62 @@
+import React from "react";
+import './navbar.css';
+import logo from "../../logo.svg";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+import LandingPage from "../Landing/landing";
+import LoginPage from "../Login/login";
+import ProfilePage from "../Profile/profile";
+import SearchPage from "../Search/search";
+import ResultsPage from "../Results/results";
+import DashboardPage from "../Dashboard/dashboard";
+import FavoritesPage from "../Favorites/favorites";
+
+function Navbar() {
+    return (
+        <div>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <a className="navbar-brand" href="http://google.com" target="_blank" rel="noopener noreferrer">
+            <img src={logo} width="30" height="30" alt="Home" />
+          </a>
+          <Link to="/" className="navbar-brand">TBH</Link>
+          <div className="collapse navbar-collapse">
+            <ul className="navbar-nav mr-auto">
+              <li className="navbar-item">
+                <Link to="/" className="nav-link">Home</Link>
+              </li>
+              <li className="navbar-item">
+                <Link to="/login" className="nav-link">Login</Link>
+              </li>
+              <li className="navbar-item">
+                <Link to="/profile" className="nav-link">Profile</Link>
+              </li>
+              <li className="navbar-item">
+                <Link to="/search" className="nav-link">Search</Link>
+              </li>
+              <li className="navbar-item">
+                <Link to="/results" className="nav-link">Results</Link>
+              </li>
+              <li className="navbar-item">
+                <Link to="/dashboard" className="nav-link">Dashboard</Link>
+              </li>
+              <li className="navbar-item">
+                <Link to="/favorites" className="nav-link">Favorites</Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <div className="container">
+        <h2>TBH App</h2>
+          <Route path="/" exact component={LandingPage} />
+          <Route path="/login" component={LoginPage} />
+          <Route path="/profile" component={ProfilePage} />
+          <Route path="/search" component={SearchPage} />
+          <Route path="/results" component={ResultsPage} />
+          <Route path="/dashboard" component={DashboardPage} />
+          <Route path="/favorites" component={FavoritesPage} />
+          </div>
+        </div>
+    )
+}
+
+export default Navbar;
