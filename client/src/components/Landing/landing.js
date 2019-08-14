4882e931583f88b5323831369164da9221fe8643
CelisseD <43502934+CelisseD@users.noreply.github.com>
2019-08-14 02:27:26 -0400

    set up brandSeed

--- client/src/components/Landing/landing.js
+++ client/src/components/Landing/landing.js
@@ -7,13 +7,15 @@ function LandingPage() {
             <h1 className="display-4">TBH</h1>
             <p className="lead">An Honest Approach to Consumer Consciousness</p>
             <hr className="my-4" />
-            <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+            <h4>Want to know how your specific purchases impact politics, the environment, and other social causes? At TBH our mission is to help consumers make conscious decisions about where their dollars are going, so we can all leave the world a little better than we found it.</h4><br />
+
+            <h4>Use the search feature to look up specific brands and companies, to find out what organizations they donate money to, what positive social causes they support, and what conflicting causes they support.</h4><br />
 
                 <div className="col-lg-6">
                     <div className="input-group">
                         <input type="text" className="form-control" placeholder="Search for..." />
                         <span className="input-group-btn">
-                            <button className="btn btn-primary" type="button">Go!</button>
+                            <button className="btn btn-primary" type="button">Get Started</button>
                         </span>
                     </div>
                 </div>
